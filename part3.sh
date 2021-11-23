b8a9646c61cefd942220b79d7a99011a9f9f124e
theofff <94871002+theofff@users.noreply.github.com>
2021-11-23 17:59:18 +0100

    Update part3.sh

--- part3.sh
+++ part3.sh
@@ -6,4 +6,19 @@ password
 EOF
 groupadd sudo
 gpasswd -a theo sudo
-pacman -S gnome gnome-extra
+systemctl start dhcpcd
+systemctl enable dhcpcd
+pacman -S gnome gnome-extra <<EOF
+
+
+
+EOF
+touch /etc/X11/xorg.conf.d/00-keyboard.conf
+echo 'Section "InputClass"' >> /etc/X11/xorg.conf.d/00-keyboard.conf
+echo '       Identifier             "keyboard Layout"' >> /etc/X11/xorg.conf.d/00-keyboard.conf
+echo '       MatchIsKeyboard        "on"' >> /etc/X11/xorg.conf.d/00-keyboard.conf
+echo '       Option "XkbLayout"     "fr"' >> /etc/X11/xorg.conf.d/00-keyboard.conf
+echo '       Option "XkbOptions"    "grp:shifts_toggle"' >> /etc/X11/xorg.conf.d/00-keyboard.conf
+echo 'EndSection' >> /etc/X11/xorg.conf.d/00-keyboard.conf
+pacman -S sudo
+systemctl enable gdm
