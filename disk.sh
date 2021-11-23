5f2384e49223a240ade5fa582543193fa2ea17d6
theofff <94871002+theofff@users.noreply.github.com>
2021-11-23 10:54:54 +0100

    Update disk.sh

--- disk.sh
+++ disk.sh
@@ -33,27 +33,3 @@ pacstrap -i /mnt base linux linux-firmware nano xfsprogs lvm2 <<EOF
 
 EOF
 genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
-ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
-hwclock --systohc
-sed -ire 's/#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/' /etc/locale.gen
-locale-gen
-touch /etc/locale.conf
-echo "LANG=fr_FR.UTF-8" >> /etc/locale.conf
-touch /etc/vconsole.conf
-echo "KEYMAP=fr-latin1" >> /etc/vconsole.conf
-touch /etc/hostname
-echo "archlinuxlvm" >> /etc/hostname
-pacman -Sy dhcpcd
-mkinitcpio -p linux
-passwd <<EOF
-password
-password
-EOF
-pacman -Sy grub
-pacman -Sy efibootmgr
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-
-grub-mkconfig -o /boot/grub/grub.cfg
-exit
-reboot
