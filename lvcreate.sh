ad7cbcafa13c7d65629bbfd040088ae122519594
theofff <94871002+theofff@users.noreply.github.com>
2021-11-23 10:55:08 +0100

    Update lvcreate.sh

--- lvcreate.sh
+++ lvcreate.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-pvcreate /dev/sda2
-vgcreate ArchVol /dev/sda2
-lvcreate -L 2G ArchVol -n ArchRacine
-lvcreate -l 100%FREE ArchVol -n ArchHome
-mkfs.fat -F 32 /dev/sda1
-mkfs.xfs /dev/ArchVol/ArchRacine
-mkfs.xfs /dev/ArchVol/ArchHome
+arch-chroot /mnt
+ln -sf /usr/share/zoneinfo/Region/City /etc/localtime
+hwclock --systohc
+sed -ire 's/#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+touch /etc/locale.conf
+echo "LANG=fr_FR.UTF-8" >> /etc/locale.conf
+touch /etc/vconsole.conf
+echo "KEYMAP=fr-latin1" >> /etc/vconsole.conf
+touch /etc/hostname
+echo "archlinuxlvm" >> /etc/hostname
+pacman -Sy dhcpcd
+mkinitcpio -p linux
+passwd <<EOF
+password
+password
+EOF
+pacman -Sy grub
+pacman -Sy efibootmgr
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+
+grub-mkconfig -o /boot/grub/grub.cfg
+exit
+reboot
